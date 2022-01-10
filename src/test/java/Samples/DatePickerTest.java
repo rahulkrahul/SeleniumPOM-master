a60007c4228119c1ec7e1979dd4d7fa8c2bbd9ec
Rahul Kaparthi <rahul.kaparthi@regiscorp.com>
2022-01-09 20:29:43 -0600

    dynamic table

---
+++ src/test/java/Samples/DatePickerTest.java
@@ -0,0 +1,15 @@
+package Samples;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DatePickerTest {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.redbus.com/");
+    }
+}
